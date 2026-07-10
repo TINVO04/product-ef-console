0ee930a01b5199bf19594af6baf20a971a8f0a65
TINVO04 <tinvo.bh2018@gmail.com>
2026-07-10 11:35:24 +0700

    feat: add product service

---
+++ Services/ProductService.cs
@@ -0,0 +1,55 @@
+using ProductEfConsole.Models;
+using ProductEfConsole.Repositories;
+
+namespace ProductEfConsole.Services;
+
+public class ProductService
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductService(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<List<Product>> GetAllProductsAsync()
+    {
+        return await _productRepository.GetAllAsync();
+    }
+
+    public async Task<Product?> GetProductByIdAsync(int id)
+    {
+        return await _productRepository.GetByIdAsync(id);
+    }
+
+    public async Task AddProductAsync(Product product)
+    {
+        await _productRepository.AddAsync(product);
+    }
+
+    public async Task<bool> UpdateProductAsync(Product product)
+    {
+        var existingProduct = await _productRepository.GetByIdAsync(product.Id);
+
+        if (existingProduct is null)
+        {
+            return false;
+        }
+
+        await _productRepository.UpdateAsync(product);
+        return true;
+    }
+
+    public async Task<bool> DeleteProductAsync(int id)
+    {
+        var existingProduct = await _productRepository.GetByIdAsync(id);
+
+        if (existingProduct is null)
+        {
+            return false;
+        }
+
+        await _productRepository.DeleteAsync(id);
+        return true;
+    }
+}
